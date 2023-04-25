1c16bd1eabd208cf1420b9e9dcc8d92209268f67
miriyala srikanth <123526232+srikanthmiriyala017@users.noreply.github.com>
2023-04-25 09:50:21 +0530

    Create styledComponents.js

---
+++ src/components/styledComponents.js
@@ -0,0 +1,30 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(to top, #3c2940, #0b0c1e);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export const Image = styled.img`
+  height: 100px;
+  width: 100px;
+`
+export const Heading = styled.p`
+  font-family: 'Roboto';
+  font-size: 24px;
+  color: #ffffff;
+`
+export const Button = styled.button`
+  height: 30px;
+  width: 80px;
+  background-color: #06b6d4;
+  color: #ffffff;
+  border: 0px none;
+  border-radius: 4px;
+`
